1e5b9e9c7c89a807f06aa151358ed5405a647623
Alexey Lobanov <lalero2000@gmail.com>
2022-06-24 13:15:33 +0300

    reading from a file

--- main.cpp
+++ main.cpp
@@ -1,7 +1,39 @@
+#include <QtCore/QCoreApplication>
 #include "iostream"
+#include <QFile>
+#include <QDebug>
+#include <QByteArray>
+#include <QString>
+#include <QTextStream>
+#include <QStringList>
+
 using namespace std;
 
-int main()
+void readFile()
 {
-    cout<<"Hello world!"<<endl;
+    QFile file("C:/Programms/LobanovTest/Ships.txt");
+     if (!file.open(QIODevice::ReadOnly | QIODevice::Text)) {
+         cout << "Invalid file path, please specify a valid file path 'Ships.txt'" << endl;
+         cout << "My specified path - C:/Programms/LobanovTest/Ships.txt";
+         return;
+     }
+
+     QStringList lines;
+
+     QTextStream in(&file);
+     while (!in.atEnd()) {
+         QString line = in.readLine();
+         lines.append(line);
+     }
+     qDebug()<<lines;
 }
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+
+    readFile();
+
+    return a.exec();
+}
+
